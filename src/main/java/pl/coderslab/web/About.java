1f4026b50ad35b3a918ef748e407db1c075227bc
MikeNovember2021 <mr.nowacki@interia.pl>
2022-02-17 14:34:56 +0100

    wwydzielone strony Kontakt i O Aplikacji

---
+++ src/main/java/pl/coderslab/web/About.java
@@ -0,0 +1,14 @@
+package pl.coderslab.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/about")
+public class About extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/about.jsp").forward(request, response);
+    }
+}
---
+++ src/main/java/pl/coderslab/web/Contact.java
@@ -0,0 +1,15 @@
+package pl.coderslab.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/contact")
+public class Contact extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/contact.jsp").forward(request, response);
+
+    }
+}
