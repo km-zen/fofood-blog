2e6db5d19a73a48aacde0e6cdd16ff459f37d0ea
Michał Ziółkowski <emziolkow@gmail.com>
2022-02-17 15:00:17 +0100

    dodanie app do nazwy login.jsp

--- src/main/java/pl/coderslab/web/Login.java
+++ src/main/java/pl/coderslab/web/Login.java
@@ -16,7 +16,7 @@ public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+        request.getServletContext().getRequestDispatcher("/appLogin.jsp").forward(request, response);
     }
 
     @Override
