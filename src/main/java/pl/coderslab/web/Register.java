f99fb86e280ef782640a82e5b6bd18d7062d4f3a
TMolak <tomekmolak@gmail.com>
2022-02-20 14:27:50 +0100

    2-obsluga-forularza-rejestracji

--- src/main/java/pl/coderslab/web/Register.java
+++ src/main/java/pl/coderslab/web/Register.java
@@ -1,5 +1,9 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.dao.PlanDao;
+import pl.coderslab.model.Admin;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -19,15 +23,38 @@ public class Register extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         request.setCharacterEncoding("UTF-8");
 
-                    //        test pobieranych wartości:
+        //        test pobieranych wartości:
         Map<String, String[]> parameterMap = request.getParameterMap();
         for (String key : parameterMap.keySet()) {
+
             response.getWriter().println("key: " + key + "<br>");
             for (String value : parameterMap.get(key)) {
                 response.getWriter().println("value " + value+ "<br>");
+
             }
         }
-                    //        koniec testu pobieranych wartości:
+
+
+        //        koniec testu pobieranych wartości:
+
+        //nowy admin
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        Admin admin = new Admin();
+        admin.setFirstName(name);
+        admin.setLastName(surname);
+        admin.setEmail(email);
+        admin.setPassword(password);
+        AdminDao adminDao = new AdminDao();
+        adminDao.create(admin);
+
+
+
+        response.sendRedirect(request.getContextPath() + "/login");
+
 
     }
 }
